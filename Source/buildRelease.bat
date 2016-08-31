e2a0ca8461a980f5591b2c7e64aa9bcc8c3ae5d9
Jonathan Bayer <linuxgurugamer@gmail.com>
2016-08-30 21:53:45 -0400

    Initial release

---
+++ Source/buildRelease.bat
@@ -0,0 +1,54 @@
+﻿rem
+@echo off
+
+set DEFHOMEDRIVE=d:
+set DEFHOMEDIR=%DEFHOMEDRIVE%%HOMEPATH%
+set HOMEDIR=
+set HOMEDRIVE=%CD:~0,2%
+
+set RELEASEDIR=d:\Users\jbb\release
+set ZIP="c:\Program Files\7-zip\7z.exe"
+echo Default homedir: %DEFHOMEDIR%
+
+set /p HOMEDIR= "Enter Home directory, or <CR> for default: "
+
+if "%HOMEDIR%" == "" (
+set HOMEDIR=%DEFHOMEDIR%
+)
+echo %HOMEDIR%
+
+SET _test=%HOMEDIR:~1,1%
+if "%_test%" == ":" (
+set HOMEDRIVE=%HOMEDIR:~0,2%
+)
+
+d:
+cd D:\Users\jbb\github\VaporVent\Source
+
+type VaporVent.version
+set /p VERSION= "Enter version: "
+
+
+mkdir %HOMEDIR%\install\GameData\VaporVent
+mkdir %HOMEDIR%\install\GameData\VaporVent\Plugins
+mkdir %HOMEDIR%\install\GameData\VaporVent\Parts
+
+ 
+
+del /s /q %HOMEDIR%\install\GameData\VaporVent\*
+rem del /y %HOMEDIR%\install\GameData\VaporVent\Plugins
+rem del /y %HOMEDIR%\install\GameData\VaporVent\Parts
+
+copy /Y "%~dp0bin\Release\VaporVent.dll" "%HOMEDIR%\install\GameData\VaporVent\Plugins"
+xcopy ..\GameData\VaporVent\parts  %HOMEDIR%\install\GameData\VaporVent\Parts
+
+copy /Y "License.txt" "%HOMEDIR%\install\GameData\VaporVent"
+copy /Y MiniAVC.dll  "%HOMEDIR%\install\GameData\VaporVent"
+copy /Y VaporVent.version  "%HOMEDIR%\install\GameData\VaporVent"
+
+%HOMEDRIVE%
+cd %HOMEDIR%\install
+
+set FILE="%RELEASEDIR%\VaporVent-%VERSION%.zip"
+IF EXIST %FILE% del /F %FILE%
+%ZIP% a -tzip %FILE% Gamedata\VaporVent
---
+++ Source/makeSteam.cs
@@ -0,0 +1,51 @@
+﻿
+//using UnityEngine;
+//using System.Collections;
+
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+
+
+namespace vaporvent
+{
+	public class makeSteam : PartModule 
+	{
+		KSPParticleEmitter[] emitter;
+
+		void PLSteam()
+		{
+			// print ("vaporvent: started makesteam");
+			emitter = part.FindModelComponents<KSPParticleEmitter>();
+			if (emitter != null)
+			{
+				foreach(KSPParticleEmitter unit in emitter)
+				{
+					if (Vessel.Situations.PRELAUNCH == vessel.situation) 
+					{
+						unit.emit = true;
+						// print ("vaporvent: true");
+					} 
+					else 
+					{
+						unit.emit = false;
+						// print ("vaporvent: false");
+					}
+				}
+			}
+		}
+		void SlowCheck()
+		{
+            // print("vaporvent: SlowCheck");
+			InvokeRepeating("PLSteam", 0, 5F);
+		}
+
+        public override void OnStart(StartState state)
+        {
+            // print("vaporvent: OnStart");
+            base.OnStart(state);
+            SlowCheck();
+        }
+    }
+}
